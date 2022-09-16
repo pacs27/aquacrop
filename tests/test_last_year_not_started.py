c907e344346c21eee3dc77aa48c9c5097f6e4b3a
pacs27 <pacopuig1995@gmail.com>
2022-09-16 11:50:15 +0200

    added test for leap and normal years

--- tests/test_last_year_not_started.py
+++ tests/test_last_year_not_started.py
@@ -17,22 +17,43 @@ class TestLastYearNotStarted(unittest.TestCase):
 
     _weather_data = prepare_weather(_weather_file_path)
 
-    _model_os = AquaCropModel(
+    
+  
+    def test_in_leap_year(self):
+        """
+        Test leap year
+        """
+        model_os = AquaCropModel(
                 sim_start_time=f"{1983}/05/15", # test with leap year
                 sim_end_time=f"{1984}/05/14",
-                weather_df=_weather_data,
+                weather_df=self._weather_data,
                 soil=Soil(soil_type='SiltClayLoam'),
                 crop=Crop('Wheat', planting_date='05/15'),
                 initial_water_content=InitialWaterContent(),
                 irrigation_management=IrrigationManagement(irrigation_method=1, SMT=[70] * 4)
             )
-    _model_os.run_model(till_termination=True)
-  
-    def test_get_additional_information(self):
+        model_os.run_model(till_termination=True)
+        additional_info = model_os.get_additional_information()
+        
+        model_is_finished = additional_info["has_model_finished"]
+        
+        self.assertEqual(model_is_finished, True)
+    def test_in_normal_year(self):
         """
-        Tets if model is finished
+        Test normal year
+
         """
-        additional_info = self._model_os.get_additional_information()
+        model_os = AquaCropModel(
+                sim_start_time=f"{1985}/05/15", # test with leap year
+                sim_end_time=f"{1986}/05/14",
+                weather_df=self._weather_data,
+                soil=Soil(soil_type='SiltClayLoam'),
+                crop=Crop('Wheat', planting_date='05/15'),
+                initial_water_content=InitialWaterContent(),
+                irrigation_management=IrrigationManagement(irrigation_method=1, SMT=[70] * 4)
+            )
+        model_os.run_model(till_termination=True)
+        additional_info = model_os.get_additional_information()
         
         model_is_finished = additional_info["has_model_finished"]
         
