651440d13121df82261fd9fa6978f54ee6a1812a
pacs27 <pacopuig1995@gmail.com>
2023-04-13 08:35:39 +0200

    general changes

---
+++ tests/test_aquacrop_wrapper.py
@@ -0,0 +1,15 @@
+import unittest
+
+from aquacrop_wrapper.weather_data.weather import Weather
+
+import sys
+
+sys.path.append("")
+
+
+
+
+class TestAquacropWrapper(unittest.TestCase):
+    
+    def test_simple_aquacrop(self):
+        
